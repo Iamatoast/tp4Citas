1ad5a1c97dd624fde8cb1de19e0353316e2e2cbe
Iamatoast <neuy@protonmail.com>
2025-05-06 09:29:25 -0300

    Visually finished

--- src/form.jsx
+++ src/form.jsx
@@ -7,13 +7,13 @@ export default function form() {
         <h2>Crear mi Cita</h2>
         <form>
             <label>Nombre Mascota</label>
-            <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" value="" />
+            <input type="text" name="mascota" class="u-full-width" placeholder="Nombre Mascota" />
             <label>Nombre Dueño</label>
-            <input type="text" name="propietario" class="u-full-width" placeholder="Nombre dueño de la mascota" value="" />
+            <input type="text" name="propietario" class="u-full-width" placeholder="Nombre dueño de la mascota" />
             <label>Fecha</label>
-            <input type="date" name="fecha" class="u-full-width" value="" />
+            <input type="date" name="fecha" class="u-full-width" />
             <label>hora</label>
-            <input type="time" name="hora" class="u-full-width" value="" />
+            <input type="time" name="hora" class="u-full-width" />
             <label>Sintomas</label>
             <textarea name="sintomas" class="u-full-width"></textarea>
             <button type="submit" class="u-full-width button-primary">Agregar Cita</button>
